381c25481a2025cbe3b00f0155d137b35e202e5d
dforgeas <david.forgeas@gmail.com>
2014-02-01 02:02:49 +0100

    Finish filefixit

--- filefixit/filefixit.ml
+++ filefixit/filefixit.ml
@@ -4,20 +4,40 @@ let scan_int () = Scanf.scanf "%d\n" (fun x -> x)
 let scan_tuple () = Scanf.scanf "%d %d\n" (fun x y -> (x, y))
 let scan_line () = Scanf.scanf "%s@\n" (fun s -> s)
 
+let partition s c =
+	try
+		let idx = String.index s c
+		in String.sub s 0 idx, String.sub s (1 + idx) (String.length s - 1 - idx)
+	with Not_found -> s, ""
+
+let clean name =
+	let idx = String.index name '/' in
+	if idx = 0 then String.sub name 1 (String.length name - 1)
+	else raise Not_found
+
+(* necessary for defining a recursive type *)
+type folder_t = Folder of (string, folder_t) Hashtbl.t
+
+let get_folder (Folder f) = f
+
+let rec mkdirs count folder name =
+	let (part, rest) = partition name '/' in
+	let subfolder =
+		(try Hashtbl.find (get_folder folder) part
+		with Not_found ->
+			let newFolder = Folder (Hashtbl.create 7)
+			in Hashtbl.add (get_folder folder) part newFolder;
+			incr count;
+			newFolder) in
+	if String.length rest > 0
+	then mkdirs count subfolder rest
+
 let solve (n, m) ls_drive ls_todo =
-	let drive = Hashtbl.create 7 in
-	let rec mkdirs folder name =
-		(* get next path part *)
-		let part = try
-			let idx = String.index name '/'
-			in String.sub name 0 idx
-			with Not_found -> name
-		in try
-			Hashtbl.find folder part
-			with Not_found ->
-				let newFolder = Hashtbl.create 7
-				in Hashtbl.add folder part newFolder;
-				newFolder
+	let drive = Folder (Hashtbl.create 7) in
+	Array.iter (mkdirs (ref 0) drive) (Array.map clean ls_drive);
+	let count = ref 0 in
+	Array.iter (mkdirs count drive) (Array.map clean ls_todo);
+	!count
 
 let () =
 	let t = scan_int () in
@@ -26,3 +46,5 @@ let () =
 		let drive = Array.init n (fun i -> scan_line ()) in
 		let todo = Array.init m (fun i -> scan_line ()) in
 		let result = solve (n, m) drive todo in
+		Printf.printf "Case #%d: %d\n" u result
+	done
