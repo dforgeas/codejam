e14c0321f0d061d7200c985fe41535add09dea6a
dforgeas <dforgeas@users.noreply.github.com>
2014-03-13 16:05:55 +0100

    sortedmean.ml: add the naive method for testing
    The naive method constructs a unique array, sorts it, and trivially gets the mean in the middle.

--- sortedmean/sortedmean.ml
+++ sortedmean/sortedmean.ml
@@ -1,10 +1,24 @@
 let scan_int () = Scanf.scanf " %d" (fun x -> x)
+let scan_float () = Scanf.scanf " %f" (fun x -> x)
 let scan_array n =
 	let result = Array.make n 0 in
 	for i = 0 to n - 1 do
-		result.(i) <- scan_int ()
+		result.(i) <- scan_float ()
 	done;
 	result
+
+let naive_median a =
+	if Array.length a = 0 then nan else
+	let mid = Array.length a / 2 in
+	if 1 land Array.length a = 0 then (* length is even *)
+		(a.(mid) +. a.(mid - 1)) /. 2
+	else (* length is odd *)
+		a.(mid)
+
+let naive_mean a b =
+	let c = Array.append a b in
+	Array.fast_sort compare c;
+	naive_median c
 
 let () =
 	let t = scan_int () in
